4c424a20ed29861857d1370f13798d529890714a
Harshal Jadhav <bestt1315@gmail.com>
2023-04-16 08:06:18 +0530

    added nvim-tree, gitsigns, null-ls, emmet, and vim-fugitive plugins

--- .config/nvim/lua/packer-config/init.lua
+++ .config/nvim/lua/packer-config/init.lua
@@ -6,7 +6,14 @@ return require('packer').startup(function()
     use 'Mofiqul/dracula.nvim'
     use 'ojroques/nvim-hardline'
     use 'mbbill/undotree'
-    use { 'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} } }
+    use 'nvim-tree/nvim-tree.lua'
+    use 'nvim-tree/nvim-web-devicons'
+    use 'jose-elias-alvarez/null-ls.nvim'
+    use 'nvim-lua/plenary.nvim'
+    use 'mattn/emmet-vim'
+    use 'lewis6991/gitsigns.nvim'
+    use 'tpope/vim-fugitive'
+    use {'nvim-telescope/telescope.nvim', tag = '0.1.1',}
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
     use { 'iamcco/markdown-preview.nvim', run = function() vim.fn["mkdp#util#install"]() end,}
 end)
