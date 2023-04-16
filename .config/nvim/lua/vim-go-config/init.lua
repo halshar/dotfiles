9d6a10e6ac9df09391640c259f2a4c64ade39b22
Harshal Jadhav <bestt1315@gmail.com>
2023-04-16 08:09:02 +0530

    deleted undotree and vim-go configs

--- .config/nvim/lua/vim-go-config/init.lua
+++
@@ -1,5 +0,0 @@
-vim.g.go_textobj_include_function_doc = 0
-vim.g.go_metalinter_enabled = { "vet", "revive", "errcheck" }
-vim.g.go_metalinter_autosave = 1
-vim.g.go_metalinter_autosave_enabled = { "vet", "revive" }
-vim.g.go_auto_type_info = 0
