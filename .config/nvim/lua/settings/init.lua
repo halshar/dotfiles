55e8f558c74958006e0376a1f7bcbb51e2b923e4
Harshal Jadhav <bestt1315@gmail.com>
2023-06-10 20:25:48 +0530

    changed indent for 4 to 2 spaces

--- .config/nvim/lua/settings/init.lua
+++ .config/nvim/lua/settings/init.lua
@@ -7,9 +7,9 @@ set.number = true
 set.relativenumber = true
 set.scrolloff = 5
 
-set.tabstop = 4
-set.softtabstop = 4
-set.shiftwidth = 4
+set.tabstop = 2
+set.softtabstop = 2
+set.shiftwidth = 2
 set.expandtab = true
 
 set.splitbelow = true
