ebb7ac464ec851a838d0459dad9a9b6a46d6b2c1
Harshal Jadhav <bestt1315@gmail.com>
2023-09-05 16:19:30 +0530

    added autocomplete

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -121,4 +121,31 @@ return {
 			},
 		},
 	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+		},
+		config = function()
+			local cmp = require("cmp")
+
+			cmp.setup({
+				completion = {
+					autocomplete = false,
+					completeopt = "menu,menuone,preview,noselect",
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+				}),
+
+				sources = cmp.config.sources({
+					{ name = "path" },
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
 }
