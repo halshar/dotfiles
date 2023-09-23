4c8799c541789d59a1605ba1110c38eacddc6bcb
Harshal Jadhav <bestt1315@gmail.com>
2023-09-24 02:00:31 +0530

    added telescope undo extension

--- .config/nvim/lua/plugins/nvim-telescope.lua
+++ .config/nvim/lua/plugins/nvim-telescope.lua
@@ -3,12 +3,26 @@ return {
 	branch = "0.1.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+		"debugloop/telescope-undo.nvim",
 		{
 			"nvim-telescope/telescope-fzf-native.nvim",
 			build = "make",
 		},
 	},
 	config = function()
+		require("telescope").setup({
+			extensions = {
+				undo = {
+					use_delta = true,
+					side_by_side = true,
+					layout_strategy = "vertical",
+					layout_config = {
+						preview_height = 0.8,
+					},
+				},
+			},
+		})
 		require("telescope").load_extension("fzf")
+		require("telescope").load_extension("undo")
 	end,
 }
