b51ef41fd39c7d26764513ac74bbe65402c616a1
Harshal Jadhav <bestt1315@gmail.com>
2023-09-23 17:59:58 +0530

    configured textobjects

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -37,9 +37,60 @@ return {
 				"yaml",
 			},
 			sync_install = false,
-			highlight = {
+			highlight = { enable = true },
+			indent = { enable = true },
+			incremental_selection = {
 				enable = true,
-				additional_vim_regex_highlighting = false,
+				keymaps = {
+					node_incremental = "v",
+					node_decremental = "V",
+				},
+			},
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["aa"] = "@parameter.outer",
+						["ia"] = "@parameter.inner",
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+						["al"] = "@loop.outer",
+						["il"] = "@loop.inner",
+						["at"] = "@comment.outer",
+					},
+				},
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["]m"] = "@function.outer",
+						["]]"] = "@class.outer",
+					},
+					goto_next_end = {
+						["]M"] = "@function.outer",
+						["]["] = "@class.outer",
+					},
+					goto_previous_start = {
+						["[m"] = "@function.outer",
+						["[["] = "@class.outer",
+					},
+					goto_previous_end = {
+						["[M"] = "@function.outer",
+						["[]"] = "@class.outer",
+					},
+				},
+				swap = {
+					enable = true,
+					swap_next = {
+						["<leader>a"] = "@parameter.inner",
+					},
+					swap_previous = {
+						["<leader>A"] = "@parameter.inner",
+					},
+				},
 			},
 		})
 	end,
