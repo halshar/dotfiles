f5897684e2c785adfdd49204d0d28b0f0032bfa6
Harshal Jadhav <bestt1315@gmail.com>
2023-11-10 17:58:50 +0530

    fix the installation error for markdown preview

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,9 +1,8 @@
 return {
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	build = "cd app && npm install",
-	init = function()
-		vim.g.mkdp_filetypes = { "markdown" }
-	end,
 	ft = { "markdown" },
+	build = function()
+		vim.fn["mkdp#util#install"]()
+	end,
 }
