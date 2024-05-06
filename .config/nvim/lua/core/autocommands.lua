f82763229b0ec2e88af0914200f8708cc4d26327
Harshal Jadhav <bestt1315@gmail.com>
2024-05-06 12:09:48 +0530

    fix: added conform auto-save autocommands

--- .config/nvim/lua/core/autocommands.lua
+++ .config/nvim/lua/core/autocommands.lua
@@ -1,7 +1,27 @@
+-- highlight the yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
 	end,
+})
+
+-- toggle formatter
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+	if args.bang then
+		-- FormatDisable! will disable formatting just for this buffer
+		vim.b.disable_autoformat = true
+	else
+		vim.g.disable_autoformat = true
+	end
+end, {
+	desc = "Disable autoformat-on-save",
+	bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
+end, {
+	desc = "Re-enable autoformat-on-save",
 })
--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,10 +3,13 @@ return {
 	config = function()
 		local conform = require("conform")
 		conform.setup({
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
+			format_on_save = function(bufnr)
+				-- Disable with a global or buffer-local variable
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return { timeout_ms = 500, lsp_fallback = true }
+			end,
 			formatters_by_ft = {
 				go = { "goimports-reviser", "golines", "gofumpt" },
 				javascript = { "prettier", "rustywind" },
