8f01b7649561a790866f90292355352049b7bff6
Harshal Jadhav <bestt1315@gmail.com>
2024-08-20 22:00:51 +0530

    fix: removed snippets support

--- stow/nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ stow/nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -5,14 +5,9 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-buffer",
-		"saadparwaiz1/cmp_luasnip",
-		"rafamadriz/friendly-snippets",
-		"L3MON4D3/LuaSnip",
 	},
 	config = function()
 		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		require("luasnip.loaders.from_vscode").lazy_load()
 
 		cmp.setup({
 			preselect = cmp.PreselectMode.None,
@@ -24,33 +19,13 @@ return {
 				autocomplete = false,
 				completeopt = "menu,menuone,preview,noinsert",
 			},
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
 			mapping = cmp.mapping.preset.insert({
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
-				["<C-l>"] = cmp.mapping(function(fallback)
-					if luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<C-h>"] = cmp.mapping(function(fallback)
-					if luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
 			}),
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
 				{ name = "path" },
 			}, {
 				{
