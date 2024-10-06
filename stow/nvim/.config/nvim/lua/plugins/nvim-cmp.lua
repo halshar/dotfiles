d0fdee8add101da55311fd97afd3c91107fdebc5
Harshal Jadhav <bestt1315@gmail.com>
2024-10-06 10:38:10 +0530

    feat: added cmp snippet expansion mapping

--- stow/nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ stow/nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -23,6 +23,21 @@ return {
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<C-l>"] = cmp.mapping(function(fallback)
+					if vim.snippet.active({ direction = 1 }) then
+						vim.snippet.jump(1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<C-h>"] = cmp.mapping(function(fallback)
+					if vim.snippet.active({ direction = -1 }) then
+						vim.snippet.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 			}),
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
