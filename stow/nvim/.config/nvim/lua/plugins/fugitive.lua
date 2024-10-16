f5708e0c68a13f327534ad9b3fff99e143cdf9ce
Harshal Jadhav <bestt1315@gmail.com>
2024-10-16 18:41:16 +0530

    fix(nvim): modular fugitive config

---
+++ stow/nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -0,0 +1,5 @@
+return {
+	"tpope/vim-fugitive",
+	cmd = { "G", "Git", "Gdiffsplit", "Gvdiffsplit", "Ggrep" },
+	config = function() end,
+}
--- stow/nvim/.config/nvim/lua/plugins/init.lua
+++ stow/nvim/.config/nvim/lua/plugins/init.lua
@@ -1,5 +1,4 @@
 return {
 	"mattn/emmet-vim",
 	"nvim-lua/plenary.nvim",
-	"tpope/vim-fugitive",
 }
