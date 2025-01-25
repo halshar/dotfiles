67765041c479fdafbb58cd9d7c52c9f05c22882b
Harshal Jadhav <bestt1315@gmail.com>
2025-01-26 01:41:51 +0530

    feat(nvim): catppuccin theme

--- stow/nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ stow/nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,40 @@
 return {
-	"EdenEast/nightfox.nvim",
-	lazy = false,
+	"catppuccin/nvim",
+	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		require("nightfox").setup({})
-		vim.cmd("colorscheme carbonfox")
+		require("catppuccin").setup({
+			flavour = "mocha",
+			dim_inactive = { enabled = false },
+			transparent_background = false,
+			show_end_of_buffer = true,
+			term_colors = false,
+			no_italic = true,
+			no_bold = false,
+			no_underline = false,
+			styles = {
+				comments = {},
+				conditionals = {},
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+				operators = {},
+				miscs = {},
+			},
+			default_integrations = true,
+			integrations = {
+				diffview = true,
+				fidget = true,
+				lsp_trouble = true,
+				mason = true,
+			},
+		})
+		vim.cmd.colorscheme("catppuccin-mocha")
 	end,
 }
