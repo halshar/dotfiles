3ff27bfe92f6401f14c7bbb17be9410bf1b70851
Harshal Jadhav <bestt1315@gmail.com>
2026-03-17 21:09:56 +0530

    feat(nvim): updated fidget plugin config

--- stow/nvim/.config/nvim/lua/plugins/fidget.lua
+++ stow/nvim/.config/nvim/lua/plugins/fidget.lua
@@ -1,13 +1,4 @@
 return {
 	"j-hui/fidget.nvim",
-	opts = {
-		progress = {
-			ignore = { "null-ls" },
-		},
-		integration = {
-			["nvim-tree"] = {
-				enable = false,
-			},
-		},
-	},
+	opts = {},
 }
